4c3226e21688c4872f892ee90b10166ee675c05b
Baris Sencan <run32a50mch@gmail.com>
2014-01-06 17:58:57 +0200

    Ticket register date, verbose name improvement and searching in owner name and ticket number fields.

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from main.models import *
 
 
-admin.site.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    search_fields = ['owner_name', 'ticket_number']
+
+admin.site.register(Ticket, TicketAdmin)
--- main/models.py
+++ main/models.py
@@ -9,3 +9,7 @@ class Ticket(models.Model):
     owner_name = models.CharField(max_length=60)
     owner_gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     ticket_number = models.CharField(max_length=50)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.owner_name + ' (' + self.ticket_number + ')'
