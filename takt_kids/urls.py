b9276073432db0291d122a8f94f498110f5a1929
Baris Sencan <run32a50mch@gmail.com>
2014-01-07 12:13:26 +0200

    Switched to grappelli for admin interface.

--- takt_kids/urls.py
+++ takt_kids/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'takt_kids.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    (r'^grappelli/', include('grappelli.urls')),
     url(r'^', include(admin.site.urls)),
 )
