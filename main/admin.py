bc6bfe88b5f8509eb6d9de564cbec4bfc56eb790
Baris Sencan <run32a50mch@gmail.com>
2014-01-20 23:50:27 +0200

    Custom event administration.

--- main/admin.py
+++ main/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from main.models import *
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'start_date', 'end_date']
+    search_fields = ['name']
+
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['ticket_number', 'owner_name', 'owner_gender',
                     'register_date']
     search_fields = ['owner_name', 'ticket_number']
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Ticket, TicketAdmin)
