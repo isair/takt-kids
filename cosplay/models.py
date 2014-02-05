e579c594de99bf1b3b451adf0251b5311035514a
Baris Sencan <run32a50mch@gmail.com>
2014-02-05 17:21:16 +0200

    Updated Cosplayer model.

--- cosplay/models.py
+++ cosplay/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from main.models import Ticket
 
 
-class Cosplayer(Ticket):
-    ticket = models.OneToOneField('Ticket')
+class Cosplayer(models.Model):
+    ticket = models.OneToOneField(Ticket)
     picture = models.ImageField(upload_to='storages.backends.s3boto')
     description = models.CharField(max_length=100)
     pass
