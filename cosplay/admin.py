31ce5767cc08524724f3b50f7368c1d8636e26aa
Baris Sencan <run32a50mch@gmail.com>
2014-02-05 17:48:54 +0200

    Unicode literals. Another thumbnail test.

--- cosplay/admin.py
+++ cosplay/admin.py
@@ -6,6 +6,5 @@ from cosplay.models import *
 class CosplayerAdmin(AdminImageMixin, admin.ModelAdmin):
     list_display = ['picture', 'description']
     search_fields = ['description']
-    pass
 
 admin.site.register(Cosplayer, CosplayerAdmin)
--- cosplay/models.py
+++ cosplay/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from sorl.thumbnail import ImageField
 from main.models import Ticket
@@ -7,4 +9,10 @@ class Cosplayer(models.Model):
     ticket = models.OneToOneField(Ticket)
     picture = ImageField(upload_to='storages.backends.s3boto')
     description = models.CharField(max_length=100)
-    pass
+
+    def image_thumbnail(self):
+
+        if self.picture:
+            return '<img src="%s" width="80" height="80" />' % self.picture.version(ADMIN_THUMBNAIL).url
+
+    image_thumbnail.allow_tags = True
