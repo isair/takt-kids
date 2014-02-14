a777f69b54274bd3bc03dc1895cd4f675022f027
Baris Sencan <run32a50mch@gmail.com>
2014-02-14 11:10:31 +0200

    Improved cosplay administration interface.

--- cosplay/admin.py
+++ cosplay/admin.py
@@ -6,11 +6,22 @@ from cosplay.models import *
 
 class CosplayerAdmin(AdminImageMixin, admin.ModelAdmin):
     form = select2_modelform(Cosplayer)
-    list_display = ('picture_thumbnail', 'character_name',
+    list_display = ('event', 'picture_thumbnail', 'character_name',
                     'ticket', 'notes', 'contest_number', 'register_date')
     list_display_links = ('character_name', )
-    search_fields = ('character_name', 'ticket',
-                     'notes', 'contest_number', 'register_date')
+    search_fields = ('character_name', 'ticket', 'notes', 'contest_number')
+    list_filter = ('event', 'register_date')
     ordering = ('-register_date', )
 
+
+class VoteAdmin(admin.ModelAdmin):
+    form = select2_modelform(Vote)
+    list_display = ('event', 'voter', 'contestant', 'vote_date')
+    list_display_links = ('voter', )
+    search_fields = ('voter', 'contestant')
+    list_filter = ('event', 'vote_date')
+    ordering = ('-vote_date', )
+
+
 admin.site.register(Cosplayer, CosplayerAdmin)
+admin.site.register(Vote, VoteAdmin)
