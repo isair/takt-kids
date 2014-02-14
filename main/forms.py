c2ed972c0e69c37a2e560cb356316a56f539f68f
Baris Sencan <run32a50mch@gmail.com>
2014-02-14 10:41:36 +0200

    Removed select2 from Ticket form.

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from easy_select2 import select2_meta_factory
-from main.models import Ticket
 
 
 class TicketForm(forms.ModelForm):
     notes = forms.CharField(widget=forms.Textarea)
-    Meta = select2_meta_factory(Ticket)
