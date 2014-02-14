612a98145d9012e241b71cc967829923aa8b4c6c
Baris Sencan <run32a50mch@gmail.com>
2014-02-14 11:18:15 +0200

    Cosplay administration bug fix.

--- cosplay/admin.py
+++ cosplay/admin.py
@@ -6,20 +6,20 @@ from cosplay.models import *
 
 class CosplayerAdmin(AdminImageMixin, admin.ModelAdmin):
     form = select2_modelform(Cosplayer)
-    list_display = ('event', 'picture_thumbnail', 'character_name',
+    list_display = ('picture_thumbnail', 'character_name',
                     'ticket', 'notes', 'contest_number', 'register_date')
     list_display_links = ('character_name', )
     search_fields = ('character_name', 'ticket', 'notes', 'contest_number')
-    list_filter = ('event', 'register_date')
+    list_filter = ('register_date', )
     ordering = ('-register_date', )
 
 
 class VoteAdmin(admin.ModelAdmin):
     form = select2_modelform(Vote)
-    list_display = ('event', 'voter', 'contestant', 'vote_date')
+    list_display = ('voter', 'contestant', 'vote_date')
     list_display_links = ('voter', )
     search_fields = ('voter', 'contestant')
-    list_filter = ('event', 'vote_date')
+    list_filter = ('vote_date', )
     ordering = ('-vote_date', )
 
 
